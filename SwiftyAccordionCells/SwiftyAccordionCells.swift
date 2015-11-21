cbaaf4f01a1a6a19e9b0a871656cc3bb4c525064
Justin M Fischer <justinmfischer@gmail.com>
2015-11-20 18:39:38 -0700

    SwiftyAccordionCells

---
+++ SwiftyAccordionCells/SwiftyAccordionCells.swift
@@ -0,0 +1,55 @@
+//
+//  SwiftyAccordionCells.swift
+//  SwiftyAccordionCells
+//
+//  Created by Fischer, Justin on 9/24/15.
+//  Copyright © 2015 Justin M Fischer. All rights reserved.
+//
+
+import Foundation
+
+class SwiftyAccordionCells {
+    private (set) var items = [Item]()
+    
+    class Item {
+        var isHidden: Bool
+        var value: AnyObject
+        
+        init(_ hidden: Bool = true, value: AnyObject) {
+            self.isHidden = hidden
+            self.value = value
+        }
+    }
+    
+    class HeaderItem: Item {
+        init (value: AnyObject) {
+            super.init(false, value: value)
+        }
+    }
+    
+    func append(item: Item) {
+        self.items.append(item)
+    }
+    
+    func removeAll() {
+        self.items.removeAll()
+    }
+    
+    func expand(headerIndex: Int) {
+        self.toogleVisible(headerIndex, isHidden: false)
+    }
+    
+    func collapse(headerIndex: Int) {
+        self.toogleVisible(headerIndex, isHidden: true)
+    }
+    
+    private func toogleVisible(var headerIndex: Int, isHidden: Bool) {
+        headerIndex++
+        
+        while headerIndex < self.items.count && !(self.items[headerIndex] is HeaderItem) {
+            self.items[headerIndex].isHidden = isHidden
+            
+            headerIndex++
+        }
+    }
+}
---
+++ SwiftyAccordionCells/ViewController.swift
@@ -0,0 +1,123 @@
+//
+//  ViewController.swift
+//  SwiftyAccordionCells
+//
+//  Created by Fischer, Justin on 9/24/15.
+//  Copyright © 2015 Justin M Fischer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var enter: UIButton!
+    @IBOutlet weak var table: UITableView!
+    
+    var previouslySelectedHeaderIndex: Int?
+    var selectedHeaderIndex: Int?
+    var selectedItemIndex: Int?
+    
+    let cells = SwiftyAccordionCells()
+    
+    override func viewDidLoad() {
+        self.setup()
+    }
+    
+    func setup() {
+        self.enter.layer.cornerRadius = 4
+        
+        self.cells.append(SwiftyAccordionCells.HeaderItem(value: "Title 1"))
+        self.cells.append(SwiftyAccordionCells.Item(value: "Sub Item 1"))
+        self.cells.append(SwiftyAccordionCells.Item(value: "Sub Item 2"))
+        self.cells.append(SwiftyAccordionCells.HeaderItem(value: "Title 2"))
+        self.cells.append(SwiftyAccordionCells.Item(value: "Sub Item 1"))
+        self.cells.append(SwiftyAccordionCells.HeaderItem(value: "Title 3"))
+        self.cells.append(SwiftyAccordionCells.Item(value: "Sub Item 1"))
+        self.cells.append(SwiftyAccordionCells.Item(value: "Sub Item 2"))
+        self.cells.append(SwiftyAccordionCells.Item(value: "Sub Item 3"))
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.cells.items.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let item = self.cells.items[indexPath.row]
+        let value = item.value as? String
+        
+        if let cell = tableView.dequeueReusableCellWithIdentifier("cell") {
+            cell.textLabel?.text = value
+            
+            if item as? SwiftyAccordionCells.HeaderItem != nil {
+                cell.backgroundColor = UIColor.lightGrayColor()
+            }
+            
+            return cell
+        }
+        
+        return UITableViewCell()
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        let item = self.cells.items[indexPath.row]
+        
+        if item is SwiftyAccordionCells.HeaderItem {
+            return 60
+        } else if (item.isHidden) {
+            return 0
+        } else {
+            return 44
+        }
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let item = self.cells.items[indexPath.row]
+        
+        if item is SwiftyAccordionCells.HeaderItem {
+            if self.selectedHeaderIndex == nil {
+                self.selectedHeaderIndex = indexPath.row
+            } else {
+                self.previouslySelectedHeaderIndex = self.selectedHeaderIndex
+                self.selectedHeaderIndex = indexPath.row
+            }
+            
+            if let previouslySelectedHeaderIndex = self.previouslySelectedHeaderIndex {
+                self.cells.collapse(previouslySelectedHeaderIndex)
+            }
+            
+            if self.previouslySelectedHeaderIndex != self.selectedHeaderIndex {
+                self.cells.expand(self.selectedHeaderIndex!)
+            } else {
+                self.selectedHeaderIndex = nil
+                self.previouslySelectedHeaderIndex = nil
+            }
+            
+            self.table.beginUpdates()
+            self.table.endUpdates()
+            
+        } else {
+            if indexPath.row != self.selectedItemIndex {
+                let cell = self.table.cellForRowAtIndexPath(indexPath)
+                cell?.accessoryType = UITableViewCellAccessoryType.Checkmark
+                
+                if let selectedItemIndex = self.selectedItemIndex {
+                    let previousCell = self.table.cellForRowAtIndexPath(NSIndexPath(forRow: selectedItemIndex, inSection: 0))
+                    previousCell?.accessoryType = UITableViewCellAccessoryType.None
+                }
+                
+                self.selectedItemIndex = indexPath.row
+            }
+        }
+    }
+    
+    @IBAction func enter(sender: AnyObject) {
+        if let selectedItemIndex = self.selectedItemIndex {
+            let selectedItemValue = self.cells.items[selectedItemIndex].value as? String
+            
+            let alert = UIAlertController(title: "Current Selection", message: selectedItemValue, preferredStyle: UIAlertControllerStyle.Alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+            
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+    }
+}
